a975fdaeb7b5b1124bec7eb1df2da9920038e95d
Filip Pasternak <filip.pasternak@miquido.com>
2022-09-27 18:22:42 +0200

    Scrollable content

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ require(['zolozRealIdCore'], function (zoloz) {
     const button = document.querySelector('button');
 
     const buttonClick = async (event) => {
+        console.log('continue to camera', event);
         event.target.disabled = true;
         await zolozCore.end('next');
         event.target.disabled = false;
